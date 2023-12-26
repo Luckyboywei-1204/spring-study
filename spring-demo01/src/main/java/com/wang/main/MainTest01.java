55f0b59c7d6289c52eeedcf323e06a5eb062cf5f
ww <wangwei49@daojia-inc.com>
2023-12-26 23:48:10 +0800

    spring的bean定义多个别名(xml方式)

--- spring-demo01/src/main/java/com/wang/main/MainTest01.java
+++ spring-demo01/src/main/java/com/wang/main/MainTest01.java
@@ -3,8 +3,10 @@ package com.wang.main;
 import com.wang.service.PetStoreServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
 
 /**
  * @ClassName MainTest01
@@ -17,8 +19,28 @@ public class MainTest01 {
     private static final Logger LOGGER = LoggerFactory.getLogger(MainTest01.class);
 
     public static void main(String[] args) {
+        test03();
+    }
+
+    public static void test01() {
         ApplicationContext context = new ClassPathXmlApplicationContext("services.xml");
         LOGGER.info("MainTest01.save start");
         context.getBean(PetStoreServiceImpl.class).save();
+    }
+
+    public static void test02() {
+        // 运行时动态注册、修改、加载bean
+        GenericApplicationContext context = new GenericApplicationContext();
+        new XmlBeanDefinitionReader(context).loadBeanDefinitions("services.xml");
+        // 触发后置处理器执行
+        context.refresh();
+        context.getBean(PetStoreServiceImpl.class).save();
+    }
+
+    public static void test03() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("services.xml");
+        LOGGER.info("MainTest01.save start");
+        // context.getBean("petStoreService1", PetStoreServiceImpl.class).save();
+        context.getBean("petStoreService2", PetStoreServiceImpl.class).save();
     }
 }
